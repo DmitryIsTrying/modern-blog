b56e0a37ddf12dae98be270fc08974e9ca024a3c
Dmitry <d2013.dima@mail.ru>
2025-04-01 02:06:25 +0500

    fix: scss linting

--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,11 +1,10 @@
 import { classNames } from "@/shared/lib/classNames/classNames";
 import { Modal, ModalProps } from "@/shared/ui/Modal/Modal";
 import { LoginForm } from "../LoginForm/LoginForm";
-import cls from "./LoginModal.module.scss";
 
 export const LoginModal = ({ className, ...modalProps }: ModalProps) => {
     return (
-        <Modal lazy {...modalProps} className={classNames(cls.loginModal, {}, [className])}>
+        <Modal lazy {...modalProps} className={classNames("", {}, [className])}>
             <LoginForm />
         </Modal>
     );
