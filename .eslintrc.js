f51cd043833481eeddd105fb9305e62dd8563db7
Dmitry <d2013.dima@mail.ru>
2025-08-06 19:36:00 +0500

    fix file name ended

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,7 @@ module.exports = {
         es2021: true,
         jest: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'plugin:i18next/recommended',
-    ],
+    extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaFeatures: {
@@ -17,26 +13,18 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: [
-        'react',
-        '@typescript-eslint',
-        'i18next',
-        'react-hooks',
-    ],
+    plugins: ['react', '@typescript-eslint', 'i18next', 'react-hooks'],
     rules: {
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
         indent: [2, 4],
-        'react/jsx-filename-extension': [
-            2,
-            { extensions: ['.js', '.jsx', '.tsx'] },
-        ],
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
         'import/no-unresolved': 'off',
         'import/prefer-default-export': 'off',
         'no-unused-vars': 'off',
         'react/require-default-props': 'off',
         'react/react-in-jsx-scope': 'off',
-        'react/jsx-props-no-spreading': 'warn',
+        'react/jsx-props-no-spreading': 'off',
         'react/function-component-definition': 'off',
         'no-shadow': 'off',
         'import/extensions': 'off',
--- src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUsername/model/services/loginByUsername/loginByUsername.ts
@@ -1,7 +1,7 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { ThunkConfig } from "app/providers/StoreProvider";
-import { User, userActions } from "entities/User";
-import { USER_LOCALSTORAGE_KEY } from "shared/const/ww";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { ThunkConfig } from 'app/providers/StoreProvider';
+import { User, userActions } from 'entities/User';
+import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
 
 interface LoginByUsernameProps {
   username: string;
@@ -9,23 +9,23 @@ interface LoginByUsernameProps {
 }
 
 export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, ThunkConfig<string>>(
-  "login/loginByUsername",
-  async (authData, thunkApi) => {
-    const { extra, dispatch, rejectWithValue } = thunkApi;
+    'login/loginByUsername',
+    async (authData, thunkApi) => {
+        const { extra, dispatch, rejectWithValue } = thunkApi;
 
-    try {
-      const response = await extra.api.post<User>("/login", authData);
+        try {
+            const response = await extra.api.post<User>('/login', authData);
 
-      if (!response.data) {
-        throw new Error();
-      }
+            if (!response.data) {
+                throw new Error();
+            }
 
-      localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
-      dispatch(userActions.setAuthData(response.data));
-      return response.data;
-    } catch (e) {
-      console.log(e);
-      return rejectWithValue("error");
-    }
-  }
+            localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
+            dispatch(userActions.setAuthData(response.data));
+            return response.data;
+        } catch (e) {
+            console.log(e);
+            return rejectWithValue('error');
+        }
+    },
 );
